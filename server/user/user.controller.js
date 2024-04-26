1503253343a23a81e60e3845ed6243d9cee088fe
Yagnik Chovatiya <67455233+id-48@users.noreply.github.com>
2024-04-26 12:17:45 +0530

    add event

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -43,7 +43,7 @@ exports.addUser = async (req, res) => {
     var userSaved = await newUser.save();
 
     if (userSaved) {
-      return res.status(200).json({ status: true, message: "User registered.", user : existingUser });
+      return res.status(200).json({ status: true, message: "User registered." });
     } else {
       return res.status(200).json({ status: false, message: "Failed." });
     }
