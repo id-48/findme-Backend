c3d14a67cb465ef7f6f3ceac2eb1e2a7d89dafd9
Crystalcube 003 <129041742+CCIT003@users.noreply.github.com>
2024-05-16 18:37:46 +0530

    build mutual friend api and admin login

--- server/admin/admin.controller.js
+++ server/admin/admin.controller.js
@@ -36,6 +36,36 @@ exports.registerAdmin = async (req, res) => {
   }
 };
 
+exports.updateAdmin = async (req, res) => {
+  var { userId, adminName, email, password } = req.body;
+
+  try {
+    var existingUser = await Admin.findOne({ _id: userId });
+
+    if (existingUser) {
+      existingUser.adminName = adminName != "" ? adminName : existingUser.adminName;
+      existingUser.email = email != "" ? email : existingUser.email;
+      existingUser.password = password != "" ? password : existingUser.password;
+
+      updateSaved = await existingUser.save();
+
+      if (updateSaved)
+        res.status(200).json({ status: true, message: "Success." });
+      else {
+        res.status(200).json({ status: false, message: "Failed." });
+      }
+    } else {
+      res
+        .status(200)
+        .json({ status: true, message: "Admin is not registered." });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: false, error: error.message || "Server Error" });
+  }
+};
+ 
 exports.loginAdmin = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -68,5 +98,53 @@ exports.loginAdmin = async (req, res) => {
     return res
       .status(500)
       .json({ status: false, error: error.message || "Server Error" });
+  }
+};
+
+exports.getAllAdmin = async (req, res) => {
+  try {
+    var existingUser = await Admin.find()
+      .limit(req.query.limit)
+      .skip((req.query.pageNo - 1) * req.query.limit)
+      .sort({ createdAt: -1 });
+    var existingUserLength = await Admin.find();
+
+    if (existingUser.length > 0) {
+      res.status(200).json({
+        status: true,
+        message: "Success.",
+        totalUser: existingUserLength.length,
+        User: existingUser,
+      });
+    } else {
+      res.status(200).json({
+        status: true,
+        message: "Success.",
+        totalUser: existingUserLength.length,
+        User: [],
+      });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: false, error: error.message || "Server Error" });
+  }
+};
+
+exports.deleteAdmin = async (req, res) => {
+  try {
+    var existingUser = await Admin.findOne({ _id: req.query.userId });
+
+    if (existingUser) {
+      var deletedUser = await Admin.deleteOne({ _id: req.query.userId });
+
+      return res.status(200).json({ status: true, message: "Success!!" });
+    } else {
+      return res
+        .status(200)
+        .json({ status: false, message: "Wrong Id received." });
+    }
+  } catch (error) {
+    return res.status(500).json({ status: false, message: error.message });
   }
 };
